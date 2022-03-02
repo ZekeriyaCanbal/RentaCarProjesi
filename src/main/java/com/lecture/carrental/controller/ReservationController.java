8834051140c4e6becc11d3ab0e9e5ba1647341b7
Zekeriya CANBAL <zekeriyacanbal@gmail.com>
2022-03-02 20:48:37 +0300

    2 Mart

--- src/main/java/com/lecture/carrental/controller/ReservationController.java
+++ src/main/java/com/lecture/carrental/controller/ReservationController.java
@@ -70,4 +70,16 @@ public class ReservationController {
         map.put("Reservation added successfully!", true);
         return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
+
+    @GetMapping("/admin/auth")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String, Boolean>> addReservation(@RequestParam(value = "userId") Long userId,
+                                                               @RequestParam(value = "carId") Car carId,
+                                                               @Valid @RequestBody Reservation reservation){
+        reservationService.addReservation(reservation, userId, carId);
+        Map<String, Boolean> map = new HashMap<>();
+        map.put("Reservation added successfully!", true);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
 }
